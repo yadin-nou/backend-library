881d6b7c23dca6abe7efcbd09d79e3124b85c394
Yadin Nou <nouyadin@gmail.com>
2026-09-23 01:17:01 +1000

    get all books from db

--- adminRouter/adminRouter.js
+++ adminRouter/adminRouter.js
@@ -1,14 +1,26 @@
 import express from "express";
-import { addBook } from "../models/bookModel.js";
+import { addBook, getAllBooks } from "../models/bookModel.js";
 const adminRouter = express.Router();
 
 adminRouter.post("/addbook", async (req, res, next) => {
   try {
-    //console.log(req.body);
     const result = await addBook(req.body);
     res.json({
       status: "success",
       message: "Successully Added book!",
+      book: result,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+adminRouter.get("/book", async (req, res, next) => {
+  try {
+    const result = await getAllBooks();
+    res.json({
+      status: "success",
+      message: "recieved all abook",
       book: result,
     });
   } catch (error) {
--- models/bookModel.js
+++ models/bookModel.js
@@ -3,3 +3,6 @@ import bookSchema from "./bookSchema.js";
 export const addBook = (book) => {
   return bookSchema.insertMany(book);
 };
+export const getAllBooks = () => {
+  return bookSchema.find({});
+};
